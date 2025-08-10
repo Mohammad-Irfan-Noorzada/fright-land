321f3c57198b0eb113339a991bd6793a5ad9c9b9
Mohammad Irfan <irfan.noorzada123@gmail.com>
2025-08-10 10:37:42 +0430

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Form from './components/Form';
+import Lessors from './components/Lessors';
+import Volcano from './components/Volcano';
+import LovedPlaces from './components/LovedPlaces';
+import Cooperations from './components/Cooperations';
+import Movies from './components/Movies';
+import DeathLevel from './components/DeathLevel';
+import Potions from './components/Potions';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Form />
+      <Lessors />
+      <Volcano />
+      <LovedPlaces />
+      <Cooperations />
+      <Movies />
+      <DeathLevel />
+      <Potions />
+      <Testimonials />
+      <Footer />  
+    </>
+  )
+}
+
+export default App;
